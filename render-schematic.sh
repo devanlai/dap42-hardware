feac390c859909745a3c25cd8495b5f58aa4c335
Devan Lai <devan.lai@gmail.com>
2016-01-09 03:11:56 -0800

    Debug renderer

--- render-schematic.sh
+++ render-schematic.sh
@@ -2,10 +2,10 @@
 if [ -z "$1" ]; then
 	echo "Pass in filename without .dch extension"
 else
-	docker run -i --rm --entrypoint="/tmp/bin/export-asc.sh" devanlai/diptrace-export:beta < $1.dch > $1.asc
+	docker run -i --rm --entrypoint="/tmp/bin/export-asc.sh" devanlai/diptrace-export:debug < $1.dch > $1.asc
 	./remove-scaling.py < $1.asc > $1.filtered.asc
-	docker run -i --rm --entrypoint="/tmp/bin/import-asc.sh" devanlai/diptrace-export:beta < $1.filtered.asc > $1.filtered.dch
-	docker run -i --rm devanlai/diptrace-export:beta $1 < $1.filtered.dch > temp.zip
+	docker run -i --rm --entrypoint="/tmp/bin/import-asc.sh" devanlai/diptrace-export:debug < $1.filtered.asc > $1.filtered.dch
+	docker run -i --rm devanlai/diptrace-export:debug $1 < $1.filtered.dch > temp.zip
 	mkdir -p renders/
 	unzip -j -d renders/$1/ temp.zip
 	mogrify -define png:exclude-chunks=date,time -strip -format png renders/$1/*.bmp
